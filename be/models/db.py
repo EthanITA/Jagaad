7d925cf5292b17a9e07bb9a6fbe77467b39ef1db
madong <viaimperiale@gmail.com>
2023-07-08 22:48:27 +0200

    refactor(be): remove unused env variable

--- be/models/db.py
+++ be/models/db.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-assert os.getenv('DATABASE_URL') is not None
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://myuser:mypassword@localhost:5432/mydb"
 
 db = SQLAlchemy(app)
