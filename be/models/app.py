aa3de9e31078020943fc8127a339db31e961f3f6
madong <viaimperiale@gmail.com>
2023-07-08 22:36:07 +0200

    fix(be): db url typo

--- be/models/app.py
+++ be/models/app.py
@@ -6,6 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 assert os.getenv('DATABASE_URL') is not None
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://myuser:mypasswordlocalhost:5432/mydb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://myuser:mypassword@localhost:5432/mydb"
 
 db = SQLAlchemy(app)
