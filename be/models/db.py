3a94c79f169be540887f19ab188d1988b7f3f516
madong <viaimperiale@gmail.com>
2023-07-08 23:35:09 +0200

    fix(be): db url issue in Docker's container

--- be/models/db.py
+++ be/models/db.py
@@ -5,6 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://myuser:mypassword@localhost:5432/mydb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://myuser:mypassword@db:5432/mydb"
 
 db = SQLAlchemy(app)
